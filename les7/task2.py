15f0e231877a430140ded4d40654e6bc28baed25
Irina Cherkaska <amadare55555@gmail.com>
2022-05-29 01:29:52 +0300

    add less 7

---
+++ les7/task2.py
@@ -0,0 +1,21 @@
+# Input data:
+stock = {
+    "banana": 6,
+    "apple": 0,
+    "orange": 32,
+    "pear": 15
+}
+prices = {
+    "banana": 4,
+    "apple": 2,
+    "orange": 1.5,
+    "pear": 3
+}
+# Create a function which takes as input two dicts with structure mentioned above
+# then computes and returns the total price of stock.
+def total_price(s, p):
+    sum = 0
+    for n in s.keys():
+        sum += s[n] * p[n]
+    print(sum)
+total_price(stock, prices)
